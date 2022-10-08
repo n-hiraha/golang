afeb53dc0439792b50e3d4e69bb6789805112065
n-hirahara <genenao21@gmail.com>
2022-10-08 22:19:27 +0900

    add: テーブル取得ロジックを追加

--- main.go
+++ main.go
@@ -3,57 +3,32 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/n-hiraha/golang/config"
 	"log"
 	"net"
-	"net/http"
 	"os"
 
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
-	}
-	url := fmt.Sprintf("http://%s", l.Addr().String())
-	log.Printf("start with: %v", url)
-	if err := run(context.Background(), l); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminated server: %v", err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, l net.Listener) error {
-	s := &http.Server{
-		// 引数で受け取ったnet.Listenerを利用するので、
-		// Addrフィールドは指定しない
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
 	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		// ListenAndServeメソッドではなく、Serveメソッドに変更する
-		if err := s.Serve(l); err != nil &&
-			// http.ErrServerClosed は
-			// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
-	// グレースフルシャットダウンの終了を待つ。
-	return eg.Wait()
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
